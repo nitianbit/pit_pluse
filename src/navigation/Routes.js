bbc75f333ca38c00e757f762cd18a355a4b72a60
Shubham <shubham@logicfirst.io>
2024-09-22 12:06:25 +0530

    dark mode added

--- src/navigation/Routes.js
+++ src/navigation/Routes.js
@@ -1,9 +1,10 @@
 // Import necessary dependencies
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { DarkTheme, DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavName } from './constants';
 import { Splash } from '../screens';
+import {useColorScheme} from 'react-native';
 
 
 
@@ -11,9 +12,12 @@ const Stack = createStackNavigator();
 
 
  const Routes = (props) => {
+    const scheme = useColorScheme();
+    const MyTheme = scheme === 'dark' ? DarkTheme : DefaultTheme;
+
 
     return (
-        <NavigationContainer>
+        <NavigationContainer theme={MyTheme}>
             <Stack.Navigator initialRouteName={NavName.SPLASH} >
                 <Stack.Screen name={NavName.SPLASH} component={Splash} options={{ headerShown: false }} />
             </Stack.Navigator>
